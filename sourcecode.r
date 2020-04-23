d3542febf0105a2f34bf2d023c4f4fa14522e029
NgenzSky <ardianarian18@yahoo.co.id>
2020-04-23 16:53:34 +0700

    data covid

--- sourcecode.r
+++ sourcecode.r
@@ -1,1 +1,15 @@
-#Data Cleaning
+library(tidyverse)
+library(data.table)
+library(bbplot)
+covid <- read.csv("C:/Users/RianAP/Desktop/Daming/daming/covid_19_clean_complete.csv")
+location_df <- function(row) {
+  if (row["COUNTRY"] == "Mainland China") {
+    if (row["STATE"] == "Hubei") {
+      return("Hubei")
+    } else {
+      return("Other Chinese Provinces")
+    } 
+  } else {
+    return("Rest of the World")
+  }
+}
