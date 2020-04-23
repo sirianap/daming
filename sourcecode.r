29f7b537bdcfbf438c3759bfc456c9ef8bcbfc0c
NgenzSky <ardianarian18@yahoo.co.id>
2020-04-23 17:13:24 +0700

    cleaning

--- sourcecode.r
+++ sourcecode.r
@@ -2,14 +2,6 @@ library(tidyverse)
 library(data.table)
 library(bbplot)
 covid <- read.csv("C:/Users/RianAP/Desktop/Daming/daming/covid_19_clean_complete.csv")
-location_df <- function(row) {
-  if (row["COUNTRY"] == "Mainland China") {
-    if (row["STATE"] == "Hubei") {
-      return("Hubei")
-    } else {
-      return("Other Chinese Provinces")
-    } 
-  } else {
-    return("Rest of the World")
-  }
-}
+df <- subset(covid, Country.Region == "China" | Country.Region == "Indonesia")
+
+             
